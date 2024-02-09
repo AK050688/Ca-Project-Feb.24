51a02a0075208a84d9a33f9817a0289f3462d6f3
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2024-02-09 23:10:27 +0530

    By Nishant Sharma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Parties from './pages/parties/Parties';
+import Items from './pages/Items/Items';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Parties />}></Route>
+        <Route path='/items' element={<Items />}></Route>
+      </Routes>
+      {/* <Parties /> */}
+      {/* <Items /> */}
     </div>
   );
 }
